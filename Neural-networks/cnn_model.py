35c6163508c8108e94a0a3babb61272ce4cccd6b
Hassan Ali <17021308@student.hhs.nl>
2020-01-13 22:34:25 +0100

    updated cnn_model.py

--- Neural-networks/cnn_model.py
+++ Neural-networks/cnn_model.py
@@ -13,8 +13,11 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from matplotlib.pyplot import imread, imshow, subplots, show
 
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import json
 
@@ -27,12 +30,12 @@ patient_groups = [
 ]
 
 
-def CNN_new(train_x, train_y, validation_x, validation_y):
+def CNN_new(train_x, train_y, valid_x, valid_y, test_x, test_y):
 
     #preview:
-    plt.figure(figsize=(10,10))
-    for i in range(25):
-        plt.subplot(5,5,i+1)
+    plt.figure(figsize=(3,3))
+    for i in range(3):
+        # plt.subplot(5,5,i+1)
         plt.xticks([])
         plt.yticks([])
         plt.grid(False)
@@ -101,27 +104,75 @@ def CNN_new(train_x, train_y, validation_x, validation_y):
 
     history = model.fit(train_x, train_y,
                         epochs=20,
-                        validation_data=(validation_x, validation_y),
-                        batch_size=32).history
-                        # callbacks=[Optimize_cnn.earlystopping]).history
+                        validation_data=(valid_x, valid_y),
+                        batch_size=32,
+                        callbacks=[Optimize_cnn.earlystopping]).history
 
     #plotting the loss and the accuracy of the model
     plt.plot(history['loss'], label='loss')
     plt.plot(history['val_loss'], label='val_loss')
-    
-    # plt.plot(history['accuracy'], label='accuracy')
-    # plt.plot(history['val_accuracy'], label='val_accuracy')
+   
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training/validation loss', loc='center')
+    plt.legend(loc='upper right')
+    plt.show()
+
+
+    plt.plot(history['accuracy'], label='accuracy')
+    plt.plot(history['val_accuracy'], label='val_accuracy')
 
     plt.xlabel('Epoch')
     plt.ylabel('accuracy')
-    plt.title('Data-resampled-color', loc='center')
-    plt.legend(loc='lower right')
+    plt.title('Training/validation accuracy', loc='center')
+    plt.legend(loc='upper right')
+    
     plt.show()
 
-    val_loss, val_accuracy = model.evaluate(validation_x, validation_y, verbose=2)
+    val_loss, val_accuracy = model.evaluate(valid_x, valid_y, verbose=2)
 
     print(val_loss, val_accuracy)
 
+    # Evaluate the model on the test data 
+    print('\n# Evaluate on test data')
+    results = model.evaluate(test_x, test_y, batch_size=32)
+    print('test loss, test acc:', results)
+
+    # Generate predictions (probabilities -- the output of the last layer)
+
+    # on new data using `predict`
+    print('\n# Generate predictions for test samples')
+    predictions = model.predict(test_x)
+    print('predictions shape:', predictions.shape)
+
+    #convert the one-hot-encoded predictions to normal label, I use of argmax function:
+    predictions = tf.argmax(predictions, axis = 1)
+
+    #To compute the confusion matrix i use the tf.math.confusion_matrix
+    matrix = tf.math.confusion_matrix(labels=test_y, predictions=predictions, num_classes=output_size)
+
+    #Plotting the confusion_matrix
+    plt.figure(figsize=(3,3))
+    sns.heatmap(matrix, annot=True, fmt=".3f", linewidths=.2, square = True, cmap = 'Blues_r')
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+    title = 'CNN of two convolutional_layers'
+    plt.title(title, size = 20)
+
+    plt.show()
+
+    #Caluculating precision, recall and the F1-score:
+
+    TP = tf.math.count_nonzero(predictions * test_y)
+    TN = tf.math.count_nonzero((predictions - 1) * (test_y - 1))
+    FP = tf.math.count_nonzero(predictions * (test_y - 1))
+    FN = tf.math.count_nonzero((predictions - 1) * test_y)
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    tf.print("precision:", precision, "recall:", recall, "f1:", f1)
 
 #generating dataset: generate_data_resampled_colour
 X, y = generate_data_resampled_colour(patient_groups)
@@ -151,8 +202,13 @@ data_generator = ImageDataGenerator(rotation_range=90,
 
 
 
-#splitting the dataset, if data augmentation is used: change the X and y to dataset_x and dataset_y
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=420)
+#splitting the into train and test dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=420)
 
 
-CNN_new(X_train, y_train, X_test, y_test)
+#splitting the train verder dataset into validation and train dataset
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.20, random_state=420)
+
+print(X_train.shape, X_test.shape, X_val.shape)
+
+CNN_new(X_train, y_train, X_val, y_val, X_test, y_test)
